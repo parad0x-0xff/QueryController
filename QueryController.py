f68736c25ad776db1b30348219b13b93d0f0880e
Parad0x <at.ferreira@protonmail.com>
2023-03-15 00:14:54 -0300

    First commit

---
+++ QueryController.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+import argparse
+from os.path import exists
+from urllib.parse import urlparse, parse_qs, urlencode
+
+parser = argparse.ArgumentParser(description='Modify the values of query parameters in a list of URLs.')
+parser.add_argument('-f', '--file', help='the file containing the list of URLs to modify.')
+parser.add_argument('-q', '--query', help='the file containing the list of parameters to filter.', default=False)
+parser.add_argument('-p', '--payload', help='the payload to substitute.', default='W00t')
+args = parser.parse_args()
+
+def parse_query_params(wordlist):
+    modified_url = []
+    for url in wordlist:
+        parsed_url = urlparse(url)
+        if not parsed_url.query:
+            continue
+        if '.css' in parsed_url.path or '.js' in parsed_url.path:
+            continue
+        query_params = parse_qs(parsed_url.query, keep_blank_values=True, strict_parsing=False, encoding='utf-8', errors='replace', max_num_fields=None)
+        updated_url = modify_query_params(parsed_url, query_params, modified_url)
+    return updated_url
+
+def modify_query_params(parsed_url, query_params, modified_url):
+    if args.query != False and exists(f'{args.query}'):
+
+        for i, key in enumerate(query_params):
+            filtered_params = open(f'{args.query}')
+            for query in filtered_params.readlines():
+                if key != query.strip():
+                    continue
+                else:
+                    query_params[key] = args.payload
+                    updated_url = parsed_url._replace(query=urlencode(query_params, doseq=True)).geturl()
+                    modified_url.append(updated_url)
+                    query_params[key] = parse_qs(parsed_url.query, keep_blank_values=True)[key][0]
+    
+    elif args.query == False or not exists(f'{args.query}'):
+        
+        for i, key in enumerate(query_params):
+            query_params[key] = args.payload
+            updated_url = parsed_url._replace(query=urlencode(query_params, doseq=True)).geturl()
+            modified_url.append(updated_url)
+            query_params[key] = parse_qs(parsed_url.query, keep_blank_values=True)[key][0]
+    else:
+        parser.error('Query file not found...')
+
+    return modified_url
+
+
+def main():
+    if not exists(f'{args.file}'):
+        parser.error('Input file is missing...')
+
+    with open(f'{args.file}', 'r') as f:
+        wordlist = [line.strip() for line in f.readlines()]
+
+    modified_urls = parse_query_params(wordlist)
+    
+    for url in modified_urls:
+        print(url)
+
+if __name__ == '__main__':
+     main()
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # QueryController
 A tool that puts you in full control of query strings.
+
+
+## **pt-br:**
+QueryController, é uma ferramenta que tem por objetivo
+trocar os valores de cara parâmetro de uma lista de URLs para um determinado valor. (ex: 'W00t').
+A grande diferença entre as outras ferramentas que encontrei pelo github, é conseguir substituir
+um parâmetro por vez ou até mesmo ter o controle de quais parâmetros você quer substituir o valor.
+
+Uma coisa que identifiquei nas ferramentas existentes foi que todas elas substituiam todos os valores
+dos parâmetro de uma única vez e isso dependendo da aplicação pode quebrar o fluxo.
+
+
+## **en:**
+QueryController is a tool that aims to replace the values of each parameter from a list of URLs with a specific value (e.g. 'W00t').
+The big difference between this tool and others I found on GitHub is that it can replace one parameter at a time or 
+even allow you to control which parameters you want to replace the value for.
+
+One thing I noticed about existing tools is that they all replace all parameter values at once, 
+which, depending on the application, can break the flow.
+
+## Similar tools I've found:
+
+* [ParamChanger](https://github.com/mathis2001/ParamChanger)
+* [ParamReplace](https://github.com/Phoenix1112/ParamReplace)
+* [qsreplace](https://github.com/tomnomnom/qsreplace)
+
+## To-Do List:
+- [x] Filter only specified params.
+- [x] Change all params one at time.
+- [ ] possibility to avoid some extensions .css, .png, etc...
+- [ ] Receive input from a piped stdin.
+- [ ] Add top payloads list.
