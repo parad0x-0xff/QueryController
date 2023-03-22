ca18bccd0b4f5d747c065d2f362aef8a05b83465
Parad0x <at.ferreira@protonmail.com>
2023-03-22 15:06:28 -0300

    Receive a piped input

--- QueryController.py
+++ QueryController.py
@@ -3,11 +3,12 @@
 import argparse
 from os.path import exists
 from urllib.parse import urlparse, parse_qs, urlencode
-
+import sys
 parser = argparse.ArgumentParser(description='Modify the values of query parameters in a list of URLs.')
 parser.add_argument('-f', '--file', help='the file containing the list of URLs to modify.')
 parser.add_argument('-q', '--query', help='the file containing the list of parameters to filter.', default=False)
 parser.add_argument('-p', '--payload', help='the payload to substitute.', default='W00t')
+parser.add_argument('-i', '--input', help='receive input from a piped tool.')
 args = parser.parse_args()
 
 def parse_query_params(wordlist):
@@ -50,13 +51,17 @@ def modify_query_params(parsed_url, query_params, modified_url):
 
 
 def main():
-    if not exists(f'{args.file}'):
+    print(args.input)
+    if args.input == '-':
+        wordlist = [line.strip() for line in sys.stdin]
+        modified_urls = parse_query_params(wordlist)
+    
+    elif exists(f'{args.file}'):
+        with open(f'{args.file}', 'r') as f:
+            wordlist = [line.strip() for line in f.readlines()]
+            modified_urls = parse_query_params(wordlist)
+    else:
         parser.error('Input file is missing...')
-
-    with open(f'{args.file}', 'r') as f:
-        wordlist = [line.strip() for line in f.readlines()]
-
-    modified_urls = parse_query_params(wordlist)
     
     for url in modified_urls:
         print(url)
